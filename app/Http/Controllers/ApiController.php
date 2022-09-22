6e48b408fb7be8610d525725e8c576daa2d7cc85
yuriCoronado <mtra.yuricoronado@gmail.com>
2022-09-21 20:21:08 -0500

    api controller, migraciones, login y verificacion de usuario en bd

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -10,13 +10,40 @@ use Illuminate\Support\Facades\Hash;
 class ApiController extends Controller
 {
     public function users(Request $request){
-        // obtiene todos los registros de la tabla users
-        $users = User::all();
+        // filtrar por parámetro de activo
+        if($request->has('active')){
+            $users = User::where('active', true)->get();
+        }else{
+            // obtiene todos los registros de la tabla users
+            $users = User::all();
+        }
         // devolver como json
         return response()->json($users);
     }
 
     public function login(Request $request){
+        
+        // mensaje de error o exito
+        $reponse = ["status" => 0, "msg" => ""];
+        
+        $data = json_decode($request->getContent());
 
+        $user = User::where('email', $data->email)->first();
+
+        if($user){
+            if(Hash::check($data->password, $user->password )){
+                $token = $user->createToken("example");
+                $reponse["status"] = 1;
+                $reponse["msg"] = $token->plainTextToken;
+            }else{
+                $reponse["msg"] = "Credenciales incorrectas";
+            }
+    
+        }else{
+            $reponse["msg"] = "Ususario no encontrado";
+        }
+
+
+        return response()->json($reponse);
     }
 }
