e765acd3f4f0629803b6ed967881c67ac41cf63b
yuriCoronado <mtra.yuricoronado@gmail.com>
2022-09-21 20:24:51 -0500

    docuemntacion

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -23,27 +23,34 @@ class ApiController extends Controller
 
     public function login(Request $request){
         
-        // mensaje de error o exito
+        // crea variable que guarda status y msj y lo inicializa como error
         $reponse = ["status" => 0, "msg" => ""];
         
+        // decodifica el contenido del request enviado con la contraseña y email
         $data = json_decode($request->getContent());
 
+        // consulta en bd si existe el email
         $user = User::where('email', $data->email)->first();
 
+        // si existe 
         if($user){
+            // desencripta y evalua si el password indicado por el usuario coincide con el de bd
             if(Hash::check($data->password, $user->password )){
+                // crea token
                 $token = $user->createToken("example");
+                // guarda mensjae de exito
                 $reponse["status"] = 1;
                 $reponse["msg"] = $token->plainTextToken;
             }else{
+                // de lo contrario guarda mensaje de error
                 $reponse["msg"] = "Credenciales incorrectas";
             }
-    
+        // si el usr no existe
         }else{
             $reponse["msg"] = "Ususario no encontrado";
         }
 
-
+        
         return response()->json($reponse);
     }
 }
